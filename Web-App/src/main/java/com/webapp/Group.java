e31652d9e652da87c1bd958239bf85d3b7d2ec21
franz <franz@Franzi.fritz.box>
2017-11-18 21:37:52 +0100

    Changed guestbook

---
+++ Web-App/src/main/java/com/webapp/Group.java
@@ -0,0 +1,17 @@
+package com.webapp;
+
+public class Group {
+  public int groupNumber;
+  public String time; //Format: 'Monday, 15:15-16:45'
+  public String place;
+  public String instructorName;
+  
+ public Group () {
+	 /*
+	  * this.groupNumber = 'Read from JSON file'
+	  * this.time = 
+	  * this.place = 
+	  * this.instructorName = 
+	  */
+ }
+}
