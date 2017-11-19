e80a8c4c600173020de97b40c8aa5d78730aa12f
franz <franz@Franzi.fritz.box>
2017-11-19 23:05:16 +0100

    Variables changed according to json file

--- Web-App/src/main/java/com/webapp/Group.java
+++ Web-App/src/main/java/com/webapp/Group.java
@@ -1,17 +1,25 @@
 package com.webapp;
 
 public class Group {
-  public int groupNumber;
-  public String time; //Format: 'Monday, 15:15-16:45'
-  public String place;
-  public String instructorName;
+  public int GroupNumber;
+  public int GroupMaxSize; 
+  public String GroupDay; //Format: 'Mo'
+  public String GroupTimeStart; //Format: '09:00'
+  public String GroupTimeEnd; //Format: '09:00'
+  public String GroupRoom; //Format: '01.11.018'
+  public String GroupTutor; 
   
  public Group () {
 	 /*
-	  * this.groupNumber = 'Read from JSON file'
-	  * this.time = 
-	  * this.place = 
-	  * this.instructorName = 
+	  * this.GroupNumber = 'Read from JSON file'
+	  * this.GroupMaxSize = 
+	  * this.GroupDay = 
+	  * this.GroupTimeStart = 
+	  * this.GroupTimeEnd =
+	  * this.GroupRoom = 
+	  * this.GroupTutor =  
 	  */
+	 
  }
+
 }
