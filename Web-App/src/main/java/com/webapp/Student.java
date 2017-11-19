53dfb99135effdb6df73e268a06da4afac19b0b8
Katharina Beck <ga24gek@mytum.de>
2017-11-19 23:30:19 +0100

    added student attributes firstname, lastname, studentID, group.

--- Web-App/src/main/java/com/webapp/Student.java
+++ Web-App/src/main/java/com/webapp/Student.java
@@ -1,6 +1,13 @@
+package com.webapp;
 
 public class Student {
-	
-
+  public String firstname;
+  public String lastname;
+  public int studentID;
+  public Group group;
+  
+  public Student(){
+	  
+  }
 
 }
